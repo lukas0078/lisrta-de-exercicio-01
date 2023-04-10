262034d36b62db503f5b42047e5eb6d45fcb77e0
lukas0078 <rufinolukas0@gmail.com>
2023-04-09 23:49:45 -0300

    primeiro commit

---
+++ App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+/* Nome do Aluno: Lukas santos rufino
+* RA: 12723134472
+* Data: 07/04/2023 */
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("ola, mundo");
+    }
+}
